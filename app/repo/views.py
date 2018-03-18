7beef574506c59f7b13766d0616eb1e35f6c5287
Sandesh <vimmrana0@gmail.com>
2018-03-18 11:45:37 +0545

    Add generic view with django template

--- app/repo/views.py
+++ app/repo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
+from .models import Repo, Category
+
+
+class HomeView(ListView):
+    template_name = "home.html"
+    context_object_name = 'repo'
+    queryset = Repo.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        # import ipdb
+        # ipdb.set_trace()
+        context['categorize_repo'] = Category.objects.all()
+        return context
